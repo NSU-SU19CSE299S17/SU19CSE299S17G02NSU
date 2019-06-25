f283065191194ff479de060d8bd26ef5044d3720
coreprinciple97 <ajishrat97@gmail.com>
2019-06-25 21:09:16 +0600

    Browser page added

--- Project_Code/Project_Code/urls.py
+++ Project_Code/Project_Code/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Home/', include('Homepage.urls'))
+    path('Home/', include('Homepage.urls')),
+    path('Browser/', include('Browser.urls'))
 ]
