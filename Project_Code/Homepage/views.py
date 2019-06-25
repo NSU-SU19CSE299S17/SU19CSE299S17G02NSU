af18b15f37530bbf7e6c36f54b4276d50ecebdcf
coreprinciple97 <ajishrat97@gmail.com>
2019-06-25 22:21:12 +0600

    homepage template and static folders added

--- Project_Code/Homepage/views.py
+++ Project_Code/Homepage/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 
 def home(request):
-    return HttpResponse("<h1> Welcome to Bookworm </h1>")
+  # return HttpResponse("<h1> Welcome to Bookworm </h1>")
+   template = loader.get_template(Homepage/index.html)
+   return render(request, 'Homepage/index.html')
+  # return HttpResponse (template.render(context,request))
