a601d30ffb1c0447cf54976ee0b6a368788684ce
coreprinciple97 <ajishrat97@gmail.com>
2019-06-29 22:43:33 +0600

    views page inside homepage updated

--- Project_Code/Homepage/views.py
+++ Project_Code/Homepage/views.py
@@ -5,7 +5,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-  # return HttpResponse("<h1> Welcome to Bookworm </h1>")
-   template = loader.get_template(Homepage/index.html)
-   return render(request, 'Homepage/index.html')
+  return render(request, 'Homepage/index.html')
   # return HttpResponse (template.render(context,request))
