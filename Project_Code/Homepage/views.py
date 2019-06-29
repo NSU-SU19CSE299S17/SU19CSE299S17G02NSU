ad6342073e43fa9bebc13485f9b7d66a6f1763e1
coreprinciple97 <ajishrat97@gmail.com>
2019-06-29 23:10:54 +0600

    Update views.py

--- Project_Code/Homepage/views.py
+++ Project_Code/Homepage/views.py
@@ -5,7 +5,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-  # return HttpResponse("<h1> Welcome to Bookworm </h1>")
- 
-   return render(request, 'Homepage/index.html')
-  # return HttpResponse (template.render(context,request))
+ return render(request, 'Homepage/index.html')
+
