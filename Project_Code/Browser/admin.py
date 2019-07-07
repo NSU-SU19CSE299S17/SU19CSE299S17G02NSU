6fb9be129db521fa2b6b4c4d1c5a41977b742d58
fatema13 <fatema.zohra01@northsouth.edu>
2019-07-07 23:55:25 +0600

    few codes added for books browser
    working on issue #30

--- Project_Code/Browser/admin.py
+++ Project_Code/Browser/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Books
+from import_export.admin import ImportExportModelAdmin
+
+
+class ViewAdmin(ImportExportModelAdmin):
+    resource_class = Books
+
+
+admin.site.register(Books, ViewAdmin)
--- Project_Code/Project_Code/settings.py
+++ Project_Code/Project_Code/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'import_export',
+
 ]
 
 MIDDLEWARE = [
