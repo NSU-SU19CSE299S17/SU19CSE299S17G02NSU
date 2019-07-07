3596f3112a66a794dabb2e05157e46598cb008ef
coreprinciple97 <ajishrat97@gmail.com>
2019-07-07 23:48:47 +0600

    solving issue #29 form updated to check validation of user input

--- Project_Code/Users/views.py
+++ Project_Code/Users/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
 #def home(request):
  # return render(request, 'Users/Register.html')
 def register(request):
-  form = UserCreationForm()
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+        username = form.cleaned_data.get('username')
+        messages.success(request, f'Account created for {username}!')
+        return redirect ('Homepage-home')
+  else:
+    form = UserCreationForm()
   return render (request,'Users/Register.html', {'form': form})
