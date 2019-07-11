bd8d09b477a6a31dfb17822472c2ef07fede206b
fatema13 <fatema.zohra01@northsouth.edu>
2019-07-11 21:09:19 +0600

    Feature: Basic html page for login page created issue#44
    Feature: Basic html page for login page created issue#44
    created the login html file

--- Project_Code/Project_Code/urls.py
+++ Project_Code/Project_Code/urls.py
@@ -28,9 +28,14 @@ urlpatterns = [
     path('Home/', include('Homepage.urls')),
     path('Browser/', include('Browser.urls')),
     path('Register/',user_views.register, name='Register'),
+<<<<<<< Updated upstream
     path('users/', include('django.contrib.auth.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+=======
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+>>>>>>> Stashed changes
 
 
 
