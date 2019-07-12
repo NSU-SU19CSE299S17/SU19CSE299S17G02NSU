5085509768e683bf249d3bade5ddb49b34e48341
coreprinciple97 <ajishrat97@gmail.com>
2019-07-13 00:10:23 +0600

    Issue #51 : Update views.py in Browser

--- Project_Code/Browser/views.py
+++ Project_Code/Browser/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Books
+from django.db.models import Q
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
 def browser(request):
-  return render(request, 'Browser/browser.html')
+  if request.method=='POST':
+      srch = request.POST['srh']
+
+      if srch:
+        match = Books.objects.filter(Q(Title__icontains=srch) | Q(Author__icontains=srch)  )
+
+        if match:
+             return render(request, 'Browser/browser.html', {'sr':match})
+        else:
+             messages.error(request,'no results found')
+
+      else:
+         return HttpResponseRedirect('/Browser/')
+
+  else:
+    return render(request, 'Browser/browser.html')
