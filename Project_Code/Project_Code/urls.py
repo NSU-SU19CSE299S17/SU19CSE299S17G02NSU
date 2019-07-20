68364c1c6bd17e913a76db306044218bc7cfa9e7
fatema13 <fatema.zohra01@northsouth.edu>
2019-07-20 22:41:43 +0600

    Feature: #32  profile page
    basic profile page can be accessed, upon registering and /or logging in.
    a default profile picture is given
    yet to add the edit options and other options

--- Project_Code/Project_Code/urls.py
+++ Project_Code/Project_Code/urls.py
@@ -32,3 +32,7 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
