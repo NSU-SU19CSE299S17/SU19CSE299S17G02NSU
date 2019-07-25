6be961bb7faba421d39b7ddd0b2f639b2db810aa
coreprinciple97 <ajishrat97@gmail.com>
2019-07-25 23:21:09 +0600

    Feature #87 : Recommender/views.py updated

--- Project_Code/Recommender/views.py
+++ Project_Code/Recommender/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def recommender(request):
+  return render(request, 'Recommender/Recommender.html')
+  return HttpResponse (template.render(context,request))
+
