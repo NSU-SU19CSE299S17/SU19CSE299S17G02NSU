429bd76ad745645dddc5f0f43294f27c79c4d8df
fatema13 <fatema.zohra01@northsouth.edu>
2019-07-26 21:51:39 +0600

    Revert "Revert "Feature #65""
    This reverts commit 1717817f5d3b0e0e6f5e3f87874b974402adf596.

--- Project_Code/Users/models.py
+++ Project_Code/Users/models.py
@@ -22,7 +22,10 @@ class Profile(models.Model):
             img.save(self.image.path)
 
 class Books(models.Model):
-    genre = models.CharField(max_length=30)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    genre = models.CharField(max_length=30, blank=False, null=False)
     author = models.CharField(max_length=30)
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30, blank=False, null=False)
 
+    def __str__(self):
+        return self.name
