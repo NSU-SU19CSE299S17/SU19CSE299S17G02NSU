96da4e532636ade88c90c746b7d48d0eab31d4d6
fatema13 <fatema.zohra01@northsouth.edu>
2019-07-26 21:37:04 +0600

    Feature #65
    imported books from .models

--- Project_Code/Users/forms.py
+++ Project_Code/Users/forms.py
@@ -25,10 +25,47 @@ class ProfileUpdateForm(forms.ModelForm):
         fields = ['image']
 
 
-class MyLibraryUpdateForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ['username', 'email']
+
+
+
+
+
+
+
+
+
+
+TRUE_FALSE_CHOICES = (
+    ('horror', 'horror'),
+    ('novel', 'novel')
+)
+
+
+
+
+
+
+
+class MyLibraryUpdateForm(forms.Form):
+
+
+    user = forms.CharField(max_length=11,)  # always 11 digit 01700000000
+    name = forms.CharField(label="Phone Number*", max_length=11,)  # always 11 digit 01700000000
+    # phone_number.widget = forms.TextInput(attrs={'size': 45, 'placeholder': 'Enter your phone number (11 digits)'})
+
+    author = forms.CharField(label="Business Name*")
+
+
+    genre = forms.ChoiceField(choices=TRUE_FALSE_CHOICES, label="Do you have a TIN?*",
+                              initial='', widget=forms.Select(), required=True)
+
+    # do_you_have_a_TIN.widget = forms.ChoiceField(attrs={'size': 45,})
+
+
+
+
+
+
 
 
 
