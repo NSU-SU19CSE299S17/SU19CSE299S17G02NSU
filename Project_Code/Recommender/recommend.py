f608d3a24359ddaeb2ad37bfa286822b69b84527
coreprinciple97 <ajishrat97@gmail.com>
2019-07-28 21:11:56 +0600

    Feature #99 : TF-IDF matrix calculated

--- Project_Code/Recommender/recommend.py
+++ Project_Code/Recommender/recommend.py
@@ -4,3 +4,11 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 book_description = pd.read_csv('.../Documentation/Dataset/DATA set.csv', encoding = 'latin-1')
 book_description = book_description[['Book_ID','Title','Author','Genre1','Genre2','Plot']]
+
+# removing the stop words
+books_tfidf = TfidfVectorizer(stop_words='english')
+# filling the missing values with empty string
+book_description['Plot'] = book_description['Plot'].fillna('')
+
+# computing TF-IDF matrix required for calculating cosine similarity
+book_description_matrix = books_tfidf.fit_transform(book_description['Plot'])
