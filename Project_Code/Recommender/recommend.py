5d08d36413fc05467f483a2632725fd4fdb64753
coreprinciple97 <ajishrat97@gmail.com>
2019-07-28 21:06:33 +0600

    Feature #99 : importing dataset csv file in a dictionary

--- Project_Code/Recommender/recommend.py
+++ Project_Code/Recommender/recommend.py
@@ -2,3 +2,5 @@ import pandas as pd
 from sklearn.metrics.pairwise import linear_kernel
 from sklearn.feature_extraction.text import TfidfVectorizer
 
+book_description = pd.read_csv('.../Documentation/Dataset/DATA set.csv', encoding = 'latin-1')
+book_description = book_description[['Book_ID','Title','Author','Genre1','Genre2','Plot']]
