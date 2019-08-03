e8823a2dc928b747b057aa41a5d0d85de9cc7e23
coreprinciple97 <ajishrat97@gmail.com>
2019-08-03 23:05:44 +0600

    Feature #65 : BookList model migrated

--- Project_Code/Users/models.py
+++ Project_Code/Users/models.py
@@ -21,11 +21,12 @@ class Profile(models.Model):
             img.thumbnail(output_size)
             img.save(self.image.path)
 
-class Books(models.Model):
+class BookList(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50, blank=False, null=False)
     genre = models.CharField(max_length=50, blank=False, null=False)
     author = models.CharField(max_length=50)
-    name = models.CharField(max_length=50, blank=False, null=False)
+
 
     def __str__(self):
         return self.name
--- Project_Code/Users/views.py
+++ Project_Code/Users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
-from Users.models import Books
+from .models import BookList
 from .forms import UserRegisterForm,  UserUpdateForm, ProfileUpdateForm, MyLibraryUpdateForm
 from django.shortcuts import render_to_response
 from django.db.models import Q
