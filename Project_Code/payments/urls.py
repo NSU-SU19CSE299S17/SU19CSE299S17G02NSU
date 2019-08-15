3e3522b45c5d247771f5ca0df201acac9a4e96b4
fatema13 <fatema.zohra01@northsouth.edu>
2019-08-15 23:51:31 +0600

    Feature #110 urls.py edited

--- Project_Code/payments/urls.py
+++ Project_Code/payments/urls.py
@@ -1,8 +1,10 @@
+from django.conf import settings
+
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
     path('charge/', views.charge, name='charge'),  # new
-    path('', views.payments, name='payments'),
+    path('', views.HomePageView.as_view(), name='payments'),
 ]
