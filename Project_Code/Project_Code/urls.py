3405fdc4f1045e86e645bfc36aab6da16f28ff9b
fatema13 <fatema.zohra01@northsouth.edu>
2019-08-15 13:05:25 +0600

    Feature #110  url added in urls.py

--- Project_Code/Project_Code/urls.py
+++ Project_Code/Project_Code/urls.py
@@ -32,6 +32,8 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('mylibrary/', user_views.mylibrary, name='mylibrary'),
     path('Recommender/', include('Recommender.urls')),
+    path('payment/', user_views.mylibrary, name='payment'),
+
 ]
 
 
