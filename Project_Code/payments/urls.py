2697cec72190e9cff7458a5ef1b6b22e27d6342b
fatema13 <fatema.zohra01@northsouth.edu>
2019-08-15 20:32:21 +0600

    Feature #110  urls.py updated

--- Project_Code/payments/urls.py
+++ Project_Code/payments/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from . import views
-from .views import viewBooks
-from .models import MyLibraryList
 
+from . import views
+
+urlpatterns = [
+    path('', views.HomePageView.as_view(), name='payments'),
+]
