caa120b094947a3c50ca1ffceacf3cf5de44dbf1
fatema13 <fatema.zohra01@northsouth.edu>
2019-08-15 20:33:11 +0600

    Feature #110  views.py updated

--- Project_Code/payments/views.py
+++ Project_Code/payments/views.py
@@ -1,13 +1,10 @@
-from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from django.conf import settings
 
-# Create your views here.
+class HomePageView(TemplateView):
+    template_name = 'payments.html'
 
-def charge(request): # new
-    if request.method == 'POST':
-        charge = stripe.Charge.create(
-            amount=500,
-            currency='usd',
-            description='A Django charge',
-            source=request.POST['stripeToken']
-        )
-        return render(request, 'charge.html')
+    def get_context_data(self, **kwargs):  # new
+        context = super().get_context_data(**kwargs)
+        context['key'] = settings.STRIPE_PUBLISHABLE_KEY
+        return context
