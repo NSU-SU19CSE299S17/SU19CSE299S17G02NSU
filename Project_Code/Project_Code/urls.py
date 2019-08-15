740dbf332ba85d17f8aa19dace8c40863e724279
fatema13 <fatema.zohra01@northsouth.edu>
2019-08-15 16:31:05 +0600

    Feature #110
    imported .views

--- Project_Code/Project_Code/urls.py
+++ Project_Code/Project_Code/urls.py
@@ -20,7 +20,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include, path
 from Users import views as user_views
-from Users import views as view
+from .views import (
+    PaymentView
+)
+
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
