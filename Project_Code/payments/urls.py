617d3bd0e9966d1f4042006f71805e094e7666c1
fatema13 <fatema.zohra01@northsouth.edu>
2019-08-15 20:38:19 +0600

    Feature #110 urls.py added new lines

--- Project_Code/payments/urls.py
+++ Project_Code/payments/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('charge/', views.charge, name='charge'),  # new
     path('', views.HomePageView.as_view(), name='payments'),
 ]
