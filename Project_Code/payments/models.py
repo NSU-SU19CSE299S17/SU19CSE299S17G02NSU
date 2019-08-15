f8f6931fcae15b65d808087a654d75107d9ada0c
fatema13 <fatema.zohra01@northsouth.edu>
2019-08-15 20:53:27 +0600

    Feature #110 models.py updated

--- Project_Code/payments/models.py
+++ Project_Code/payments/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Payment(models.Model):
     stripe_charge_id = models.CharField(max_length=50)
-   # user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, blank=True, null=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, blank=True, null=True)
     amount = models.FloatField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
