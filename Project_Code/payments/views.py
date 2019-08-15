71aea5c127ec51871ffa6f1e98af516c4ddcf79d
fatema13 <fatema.zohra01@northsouth.edu>
2019-08-15 20:17:03 +0600

    Feature #110 added payments/ views.py

---
+++ Project_Code/payments/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def charge(request): # new
+    if request.method == 'POST':
+        charge = stripe.Charge.create(
+            amount=500,
+            currency='usd',
+            description='A Django charge',
+            source=request.POST['stripeToken']
+        )
+        return render(request, 'charge.html')
