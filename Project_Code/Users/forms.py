c761028a75e35f61cd98d4f3a099e3fa7b8d307c
fatema13 <fatema.zohra01@northsouth.edu>
2019-08-15 20:16:33 +0600

    Feature #110  edited users/forms.py

--- Project_Code/Users/forms.py
+++ Project_Code/Users/forms.py
@@ -59,12 +59,3 @@ class MyLibraryUpdateForm(forms.Form):
 
 
 
-
-
-class PaymentForm(forms.Form):
-    stripeToken = forms.CharField(required=False)
-    save = forms.BooleanField(required=False)
-    use_default = forms.BooleanField(required=False)
-
-
-
