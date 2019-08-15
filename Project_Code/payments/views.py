706c16082f41469399761fa60abe3e5bbe3faad9
fatema13 <fatema.zohra01@northsouth.edu>
2019-08-15 20:38:44 +0600

    Feature #110  added new function to views.py

--- Project_Code/payments/views.py
+++ Project_Code/payments/views.py
@@ -1,5 +1,10 @@
+import stripe
 from django.views.generic.base import TemplateView
 from django.conf import settings
+from django.shortcuts import render # new
+
+stripe.api_key = settings.STRIPE_SECRET_KEY # new
+
 
 class HomePageView(TemplateView):
     template_name = 'payments.html'
@@ -8,3 +13,14 @@ class HomePageView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['key'] = settings.STRIPE_PUBLISHABLE_KEY
         return context
+
+
+def charge(request): # new
+    if request.method == 'POST':
+        charge = stripe.Charge.create(
+            amount=500,
+            currency='usd',
+            description='A Django charge',
+            source=request.POST['stripeToken']
+        )
+        return render(request, 'charge.html')
